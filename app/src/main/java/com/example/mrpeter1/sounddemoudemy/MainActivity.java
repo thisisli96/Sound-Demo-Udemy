8ebeb3296ec857d56ffd1d8d52268c23813487fb
muh ikhsan <thisisli96@yahoo.com>
2019-12-06 05:43:49 +0700

    step one sound efeck run when application run

--- app/src/main/java/com/example/mrpeter1/sounddemoudemy/MainActivity.java
+++ app/src/main/java/com/example/mrpeter1/sounddemoudemy/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.mrpeter1.sounddemoudemy;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.provider.MediaStore;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        MediaPlayer mediaPlayer = MediaPlayer.create(this, R.raw.fantasy);
+        mediaPlayer.start();
+
     }
 }
