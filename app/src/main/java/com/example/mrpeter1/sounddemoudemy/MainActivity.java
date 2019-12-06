515c44a870adb1bef851cb8236ffe563e418be5a
muh ikhsan <thisisli96@yahoo.com>
2019-12-06 07:07:07 +0700

    step menambah kan volume dan sudah bisa di gunakan dengan baik

--- app/src/main/java/com/example/mrpeter1/sounddemoudemy/MainActivity.java
+++ app/src/main/java/com/example/mrpeter1/sounddemoudemy/MainActivity.java
@@ -2,16 +2,21 @@ package com.example.mrpeter1.sounddemoudemy;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.media.AudioManager;
 import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.provider.MediaStore;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.SeekBar;
 import android.widget.VideoView;
 
 public class MainActivity extends AppCompatActivity {
    Button btnplay, btnpause;
     MediaPlayer mediaPlayer;
+    AudioManager audioManager;
+
     public void play(View view){
         //mediaPlayer = MediaPlayer.create(this, R.raw.fantasy);
         mediaPlayer.start();
@@ -27,9 +32,34 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        audioManager =(AudioManager) getSystemService(AUDIO_SERVICE);
+        int maxVolume = audioManager.getStreamMaxVolume(AudioManager.STREAM_MUSIC); // mengontor agar volume itu sesuai dengan volume saat memutar musik pada handpone
+        int currentVolume = audioManager.getStreamVolume(AudioManager.STREAM_MUSIC); // untuk mengatur volume sesuai dengan seekbar vilume
+
         mediaPlayer = MediaPlayer.create(this, R.raw.fantasy);
+        SeekBar volumeseekbar = (SeekBar) findViewById(R.id.VolumeSeekBar);
         btnplay = findViewById(R.id.btnplay);
         btnpause = findViewById(R.id.btnpause);
+        volumeseekbar.setMax(maxVolume); // mengatur volumeseekbar sesuai dengan volume maxVolume
+        volumeseekbar.setProgress(currentVolume);
+
+        volumeseekbar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                Log.i("seekbar changed ", Integer.toString(progress));
+                audioManager.setStreamVolume(AudioManager.STREAM_MUSIC, progress, 0);
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
 //
 //        btnplay.setOnClickListener(new View.OnClickListener() {
 //            @Override
